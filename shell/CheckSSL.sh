72db9a4610708e38bec9e36c91b4db42b5f41c19
root <Eli-Chang>
2024-06-30 18:25:51 +0800

    新增脚本

---
+++ shell/CheckSSL.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# 加载 .env 文件
+# .env文件应和该脚本文件处于同一目录内，并且文件中应包含变量"bark_key"，例如“bark_key=xxxxxxxxxx”
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+# 定义站点列表
+sites=("aliyun.com" "elisky.cn" "github.com")
+
+# 函数来提取和格式化 SSL 证书的到期日期
+get_ssl_expiry_date() {
+    # 获取证书信息
+    cert_info=$(echo | openssl s_client -servername "$site" -connect "$site:443" -showcerts 2>&1)
+    # 提取并格式化证书信息
+    expire_date=$(echo "$cert_info" | openssl x509 -noout -dates 2>/dev/null | grep 'notAfter' | sed 's/notAfter=//')
+    # 将到期日期转换为 yyyy-MM-dd 格式
+    formatted_date=$(date -d "$expire_date" +%Y-%m-%d 2>/dev/null)
+    # 输出结果
+    # echo "$site: $formatted_date"
+        # 获取当前日期
+    current_date=$(date +%Y-%m-%d)
+    
+    # 计算证书到期日期和当前日期之间的天数
+    days_left=$(( ( $(date -d "$formatted_date" +%s) - $(date -d "$current_date" +%s) ) / (60*60*24) ))
+    
+    if [ $days_left -le 7 ]; then
+        curl -s https://api.day.app/$bark_key/SSL监控告警/"$site: 证书在 $days_left 天内过期"?group=jobtest
+    else
+        echo "$site: Certificate expires on $formatted_date"
+    fi
+}
+
+# 循环遍历所有站点
+for site in "${sites[@]}"; do
+    get_ssl_expiry_date "$site"
+done
---
+++ shell/README.md
@@ -0,0 +1,9 @@
+# 脚本说明
+> 声明
+> 系列脚本仅供学习交流使用，请勿在生产环境使用，若对于脚本有什么优化的建议，欢迎提交issue
+# CheckSSL.sh SSL证书监控脚本
+提供使用shell脚本来简单监控站点的SSL证书到期时间，若小于7天，则触发curl动作配合[bark](https://bark.day.app/#/)（该应用貌似仅Iphone可用）进行告警，也可以优化之后支持钉钉、企业微信等告警通道发送告警提醒
+## 脚本依赖
+依赖于openssl，目前常见的Linux系统默认均包含该程序
+## 变量依赖
+.env文件应和该脚本文件处于同一目录内，并且文件中应包含变量"bark_key"，例如“bark_key=xxxxxxxxxx”，获取方式参考：[brak使用参考](https://bark.day.app/#/tutorial)
