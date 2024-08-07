155efeab754369a441a3732d4cd45c3a3b5ab1c5
Eli <838785877@qq.com>
2024-08-08 01:16:52 +0800

    增加脚本集中入口

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # 定义帮助信息
 show_help() {
-    echo "Usage: $0 [command]"
+    echo "Usage: $0 [command] [download|execute]"
     echo "Available commands:"
     echo "  install-docker"
     echo "  install-docker2"
@@ -12,39 +12,55 @@ show_help() {
     echo "  mng"
     echo "  OpenSSL"
     echo "  SystemInfoMonitor"
+    echo "Options:"
+    echo "  download - Download the script but do not execute it."
+    echo "  execute - Execute the script after downloading it (default)."
 }
 
 # 检查是否有参数
-if [ $# -eq 0 ]; then
+if [ $# -lt 1 ]; then
     show_help
 else
-    case "$1" in
+    command="$1"
+    action="execute" # 默认操作是执行
+    if [ $# -eq 2 ]; then
+        action="$2"
+    fi
+
+    case "$command" in
         "install-docker")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/install-docker.sh)
+            url="https://download.elisky.cn/scripts/shell/install-docker.sh"
             ;;
         "install-docker2")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/install-docker2.sh)
+            url="https://download.elisky.cn/scripts/shell/install-docker2.sh"
             ;;
         "CheckSSL")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/CheckSSL.sh)
+            url="https://download.elisky.cn/scripts/shell/CheckSSL.sh"
             ;;
         "install-cri-docker")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/install-cri-docker.sh)
+            url="https://download.elisky.cn/scripts/shell/install-cri-docker.sh"
             ;;
         "install-nginx")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/install-nginx.sh)
+            url="https://download.elisky.cn/scripts/shell/install-nginx.sh"
             ;;
         "mng")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/mng.sh)
+            url="https://download.elisky.cn/scripts/shell/mng.sh"
             ;;
         "OpenSSL")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/OpenSSL.sh)
+            url="https://download.elisky.cn/scripts/shell/OpenSSL.sh"
             ;;
         "SystemInfoMonitor")
-            bash <(curl -sL https://download.elisky.cn/scripts/shell/SystemInfoMonitor.sh)
+            url="https://download.elisky.cn/scripts/shell/SystemInfoMonitor.sh"
             ;;
         *)
             show_help
+            exit 1
             ;;
     esac
+
+    if [ "$action" = "download" ]; then
+        curl -o "${command}.sh" -sL "$url"
+    else
+        bash <(curl -sL "$url")
+    fi
 fi
